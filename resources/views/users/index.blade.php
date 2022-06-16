aa196afd32447055d4a58f44691be9230872a629
rodhisz <rodhisz.45@gmail.com>
2022-06-16 16:41:43 +0700

    text center fo tr

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
-                <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded">
+                <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                     + Add User
                 </a>
             </div>
@@ -25,18 +25,18 @@
                     </thead>
                     <tbody>
                         @forelse($user as $item)
-                            <tr>
+                            <tr class="text-center">
                                 <td class="border px-6 py-4">{{ $item->id }}</td>
                                 <td class="border px-6 py-4 ">{{ $item->name }}</td>
                                 <td class="border px-6 py-4">{{ $item->email }}</td>
                                 <td class="border px-6 py-4">{{ $item->roles }}</td>
                                 <td class="border px-6 py- text-center">
-                                    <a href="{{ route('users.edit', $item->id) }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 mx-2 rounded">
+                                    <a href="{{ route('users.edit', $item->id) }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
                                         Edit
                                     </a>
                                     <form action="{{ route('users.destroy', $item->id) }}" method="POST" class="inline-block">
                                         {!! method_field('delete') . csrf_field() !!}
-                                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-black font-bold py-2 px-4 mx-2 rounded inline-block">
+                                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded inline-block">
                                             Delete
                                         </button>
                                     </form>
